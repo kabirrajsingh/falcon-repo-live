5cd38e7bfb40bd04cfc045f75f75e427c89520e9
Kabir Raj Singh <kabirrajsingh10@gmail.com>
2024-08-09 16:06:38 +0530

    changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     // baseURL: 'http://192.168.0.100:8000'
-    baseURL: 'https://debasmitroy.pythonanywhere.com/hello'
+    baseURL: 'https://debasmitroy.pythonanywhere.com'
 });
